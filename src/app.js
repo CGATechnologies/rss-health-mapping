4fb43a77e9b2e2e7105bc187cf33487235963067
Joshua Swiss <swissjoshua@gmail.com>
2017-01-25 17:03:07 +0300

    added highchart-ng and created directive for first dashboard component

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
-// import highcharts
+// import highcharts and highcharts-ng
 import highcharts from 'highcharts';
+import 'highcharts-ng';
 
 // import CSS, FA, and bootstrap
 import './app.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-// import NavBar
+// import NavBar and other components
 import Components from './components';
-
 
 // import config file and modules here
 import routing from './app.config';
@@ -21,23 +21,7 @@ import facility from './features/facility';
 import dashboard from './features/dashboard';
 import table from './features/data-tables';
 
-// let app = () => {
-//   return {
-//     template: require('./app.html'),
-//     controller: 'AppCtrl',
-//     controllerAs: 'app'
-//   }
-// };
-
-// class AppCtrl {
-//   constructor() {
-//     this.url = 'https://github.com/preboot/angular-webpack';
-//   }
-// }
-
-// const MODULE_NAME = 'app';
-
-angular.module('app', [ uirouter, home, facility, dashboard, table, Components ])
+angular.module('app', [ uirouter, home, facility, dashboard, table, Components, "highcharts-ng" ])
   // .directive('app', app)
   // .controller('AppCtrl', AppCtrl)
   .config(routing);
