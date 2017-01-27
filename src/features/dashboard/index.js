aea7cde798df979aafe01415e39a0e2c57e6be1d
Joshua Swiss <swissjoshua@gmail.com>
2017-01-27 11:57:34 +0300

    cleaned up extraneous files, started playing with highcharts

--- src/features/dashboard/index.js
+++ src/features/dashboard/index.js
@@ -8,10 +8,10 @@ import DashboardController from './dash.controller';
 import dataSources from './dataSources.service';
 import facTypeData from './facType.service';
 import facTypeComponent from './facType.component';
-import hChart from './test.directive';
+// import hChart from './test.directive';
 // Import other dashboard services here
 
-export default angular.module('app.dash', [ uirouter, dataSources, facTypeData, hChart ])
+export default angular.module('app.dash', [ uirouter, dataSources, facTypeData ])
   .config(routing)
   .controller('DashboardController', DashboardController)
   .name;
