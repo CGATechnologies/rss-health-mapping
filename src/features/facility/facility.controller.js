0ce1f001621b84ed8363bd5c8e5ce15ce3f372b0
Joshua Swiss <swissjoshua@gmail.com>
2017-01-31 17:18:14 +0300

    per facility staff counts up, with ng show/hide

--- src/features/facility/facility.controller.js
+++ src/features/facility/facility.controller.js
@@ -10,6 +10,8 @@ export default class FacilityController {
         self.facility = facilityResponse.data.Facility;
         self.facStaff = facilityResponse.data.Staff;
         self.staffCount = self.facStaff.length;
+
+        // Staff count by gender
         self.maleStaffCount = 0;
         self.femaleStaffCount = 0;
         self.facStaff.forEach(fac => {
@@ -20,8 +22,30 @@ export default class FacilityController {
           }
         });
 
-        // console.log(self.femaleStaffCount);
-        console.log(self.maleStaffCount);
+        // Counting roles at each facility
+        self.roleCount = d3.nest()
+          .key(function (d) { return d.nrLocalName; })
+          .rollup(function (v) { return v.length })
+          .entries(self.facStaff);
+
+        // Sorting roles alphabetically
+        self.roleCount.sort(function (a, b) {
+          var nameA = a.key.toUpperCase(); // ignore upper and lowercase
+          var nameB = b.key.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+        });
+
+        // show/hide onclick
+        self.isVisible = false;
+        self.ShowHide = function () {
+          // If DIV is visible it will be hidden, and vice versa
+          self.isVisible = self.isVisible ? false : true;
+        };
 
       });
   }
