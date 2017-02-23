73a1400e16a6b08555e8c9927807b05a364cb643
Joshua Swiss <swissjoshua@gmail.com>
2017-02-23 16:44:22 +0300

    merged;

--- src/features/home/cartoMap.service.js
+++ src/features/home/cartoMap.service.js
@@ -1,43 +1,43 @@
 import angular from 'angular';
 
-class cartoMap { 
+class cartoMap {
 
   loadMap() {
     // console.log('hitting')
     let url = 'https://cgamap.carto.com/api/v2/viz/ef0fd039-511e-4c78-a39a-a739351493e7/viz.json';
     cartodb.createVis('map', url, {
-        shareable: true,
-        title: true,
-        description: true,
-        search: true,
-        tiles_loader: true,
-        center_lat: 7.123519,
-        center_lon: 30.119062,
-        zoom: 6
+      shareable: true,
+      title: true,
+      description: true,
+      search: true,
+      tiles_loader: true,
+      center_lat: 7.123519,
+      center_lon: 30.119062,
+      zoom: 6
     })
-    .done((vis, layers) => {
+      .done((vis, layers) => {
 
-      // Set max zoom out
-      vis.map.set({
-        minZoom: 4,
-        maxZoom: 10
-      });
+        // Set max zoom out
+        vis.map.set({
+          minZoom: 6,
+          maxZoom: 10
+        });
 
-      // layer 0 is the base layer, layer 1 is cartodb layer
-      // setInteraction is disabled by default
-      layers[1].setInteraction(true);
-      layers[1].on('featureOver', (e, latlng, pos, data) => {
-        cartodb.log.log(e, latlng, pos, data);
+        // layer 0 is the base layer, layer 1 is cartodb layer
+        // setInteraction is disabled by default
+        layers[ 1 ].setInteraction(true);
+        layers[ 1 ].on('featureOver', (e, latlng, pos, data) => {
+          cartodb.log.log(e, latlng, pos, data);
+        });
+        // you can get the native map to work with it
+        var map = vis.getNativeMap();
+        // now, perform any operations you need
+        // map.setZoom(3);
+        // map.panTo([50.5, 30.5]);
+      })
+      .error(function (err) {
+        console.log(err);
       });
-      // you can get the native map to work with it
-      var map = vis.getNativeMap();
-      // now, perform any operations you need
-      // map.setZoom(3);
-      // map.panTo([50.5, 30.5]);
-    })
-    .error(function(err) {
-      console.log(err);
-    });
   }
 }
 
