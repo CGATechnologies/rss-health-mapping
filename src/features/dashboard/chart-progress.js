17276a0407511237e23b4a0f9fa6c7b466a68c26
Joshua Swiss <swissjoshua@gmail.com>
2017-03-01 15:13:14 +0300

    first progress chart showing

---
+++ src/features/dashboard/chart-progress.js
@@ -0,0 +1,92 @@
+function facProgress() {
+  const colors = {
+    'pink': '#E1499A',
+    'yellow': '#f0ff08',
+    'green': '#47e495'
+  };
+
+  const radius = 75;
+  const border = 5;
+  const padding = 15;
+  const startPercent = 0;
+  const endPercent = self.facSurveyCount / 1200;
+  const twoPi = Math.PI * 2;
+  const formatPercent = d3.format('.0%');
+  const boxSize = (radius + padding) * 2;
+
+  let count = Math.abs((endPercent - startPercent) / 0.01);
+  let step = endPercent < startPercent ? -0.01 : 0.01;
+
+  const arc = d3.svg.arc()
+    .startAngle(0)
+    .innerRadius(radius)
+    .outerRadius(radius - border);
+
+  const parent = d3.select('div#fac-survey-progress');
+
+  const svg = parent.append('svg')
+    .attr('width', boxSize)
+    .attr('height', boxSize);
+
+  const defs = svg.append('defs');
+
+  const filter = defs.append('filter')
+    .attr('id', 'blur');
+
+  filter.append('feGaussianBlur')
+    .attr('in', 'SourceGraphic')
+    .attr('stdDeviation', '7');
+
+  const g = svg.append('g')
+    .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+
+  const meter = g.append('g')
+    .attr('class', 'progress-meter');
+
+  meter.append('path')
+    .attr('class', 'background')
+    .attr('fill', '#ccc')
+    .attr('fill-opacity', 0.5)
+    .attr('d', arc.endAngle(twoPi));
+
+  const foreground = meter.append('path')
+    .attr('class', 'foreground')
+    .attr('fill', colors.green)
+    .attr('fill-opacity', 1)
+    .attr('stroke', colors.green)
+    .attr('stroke-width', 5)
+    .attr('stroke-opacity', 1)
+    .attr('filter', 'url(#blur)');
+
+  const front = meter.append('path')
+    .attr('class', 'foreground')
+    .attr('fill', colors.green)
+    .attr('fill-opacity', 1);
+
+  const numberText = meter.append('text')
+    .attr('fill', '#000000')
+    .attr('text-anchor', 'middle')
+    .attr('dy', '.35em');
+
+  function updateProgress(progress) {
+    foreground.attr('d', arc.endAngle(twoPi * progress));
+    front.attr('d', arc.endAngle(twoPi * progress));
+    numberText.text(formatPercent(progress));
+  }
+
+  let progress = startPercent;
+
+  (function loops() {
+    updateProgress(progress);
+
+    if (count > 0) {
+      count--;
+      progress += step;
+      setTimeout(loops, 10);
+    }
+  })();
+}
+
+export default {
+  facProgress: facProgress
+};
--- src/features/dashboard/dash.controller.js
+++ src/features/dashboard/dash.controller.js
@@ -1,4 +1,5 @@
 import facilityTypeCount from './chart-facility-type-count';
+import facProgress from './chart-progress';
 
 export default class DashboardController {
   constructor(dataSources, ona) {
@@ -9,7 +10,7 @@ export default class DashboardController {
 
     self.resizeEvent = function (e) {
       window.resize(e);
-    }
+    };
 
     dataSources.getAllHrisData()
       // Process data in the 'then' callback below
@@ -124,10 +125,101 @@ export default class DashboardController {
       .then(function (facSurvey) {
 
         self.facSurveyMetaData = facSurvey.data;
+        // get number of submissions from each survey
         self.facSurveyCount = self.facSurveyMetaData.filter(k => k.formid === "179398")[ 0 ].submissions;
         self.chdSurveyCount = self.facSurveyMetaData.filter(k => k.formid === "179432")[ 0 ].submissions;
         self.donorSurveyCount = self.facSurveyMetaData.filter(k => k.formid === "157221")[ 0 ].submissions;
         self.ngoSurveyCount = self.facSurveyMetaData.filter(k => k.formid === "183415")[ 0 ].submissions;
+
+        // facProgress.facProgress();
+
+        //build progress charts
+        const colors = {
+          'pink': '#E1499A',
+          'yellow': '#f0ff08',
+          'green': '#47e495'
+        };
+
+        const radius = 50;
+        const border = 5;
+        const padding = 15;
+        const startPercent = 0;
+        const endPercent = self.facSurveyCount / 1200;
+        const twoPi = Math.PI * 2;
+        const formatPercent = d3.format('.0%');
+        const boxSize = (radius + padding) * 2;
+
+        let count = Math.abs((endPercent - startPercent) / 0.01);
+        let step = endPercent < startPercent ? -0.01 : 0.01;
+
+        const arc = d3.svg.arc()
+          .startAngle(0)
+          .innerRadius(radius)
+          .outerRadius(radius - border);
+
+        const parent = d3.select('div#fac-survey-progress');
+
+        const svg = parent.append('svg')
+          .attr('width', boxSize)
+          .attr('height', boxSize);
+
+        const defs = svg.append('defs');
+
+        const filter = defs.append('filter')
+          .attr('id', 'blur');
+
+        filter.append('feGaussianBlur')
+          .attr('in', 'SourceGraphic')
+          .attr('stdDeviation', '7');
+
+        const g = svg.append('g')
+          .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+
+        const meter = g.append('g')
+          .attr('class', 'progress-meter');
+
+        meter.append('path')
+          .attr('class', 'background')
+          .attr('fill', '#ccc')
+          .attr('fill-opacity', 0.5)
+          .attr('d', arc.endAngle(twoPi));
+
+        const foreground = meter.append('path')
+          .attr('class', 'foreground')
+          .attr('fill', colors.green)
+          .attr('fill-opacity', 1)
+          .attr('stroke', colors.green)
+          .attr('stroke-width', 5)
+          .attr('stroke-opacity', 1)
+          .attr('filter', 'url(#blur)');
+
+        const front = meter.append('path')
+          .attr('class', 'foreground')
+          .attr('fill', colors.green)
+          .attr('fill-opacity', 1);
+
+        const numberText = meter.append('text')
+          .attr('fill', '#000000')
+          .attr('text-anchor', 'middle')
+          .attr('dy', '.35em');
+
+        function updateProgress(progress) {
+          foreground.attr('d', arc.endAngle(twoPi * progress));
+          front.attr('d', arc.endAngle(twoPi * progress));
+          numberText.text(formatPercent(progress));
+        }
+
+        let progress = startPercent;
+
+        (function loops() {
+          updateProgress(progress);
+
+          if (count > 0) {
+            count--;
+            progress += step;
+            setTimeout(loops, 10);
+          }
+        })();
 
       });
   }
