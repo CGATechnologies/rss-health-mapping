1333d7de7cce9a6bd83cccaaa3d508f244db7f08
Joshua Swiss <swissjoshua@gmail.com>
2017-03-07 16:22:54 +0300

    cleaned table array so it searches properly

--- src/features/data-tables/table.controller.js
+++ src/features/data-tables/table.controller.js
@@ -11,7 +11,23 @@ export default class HrisTableController {
       .then(function (dataResponse) {
         self.items = dataResponse;
         self.facilities = self.items.data;
-        self.sortType = 'staff';
+        // console.log(self.facilities);
+
+        self.cleanFac = self.facilities;
+        for (let i = 0; i < self.facilities.length; i++) {
+          delete self.cleanFac[ i ].CID;
+          delete self.cleanFac[ i ].PID;
+          delete self.cleanFac[ i ].RoleCount;
+          delete self.cleanFac[ i ].TS;
+          delete self.cleanFac[ i ].fMDA;
+          delete self.cleanFac[ i ].fPayam;
+          delete self.cleanFac[ i ].UID;
+          delete self.cleanFac[ i ].fAddress;
+          delete self.cleanFac[ i ].$$hashKey;
+        }
+        console.log(self.cleanFac);
+
+        self.sortType = '';
         self.sortReverse = false;
         self.searchHris = '';
         self.dataLoaded = true;
