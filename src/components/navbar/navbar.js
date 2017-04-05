548a8fa8068261d47fb2f92569c1fa66829aa0a6
Joshua Swiss <swissjoshua@gmail.com>
2017-04-05 09:31:30 +0300

    commented out pillars for now

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,34 +5,33 @@ class NavBarController {
 
     this.brand = 'South Sudan Health Info';
 
-    this.items = [ {
-      href: '/dashboard',
-      label: 'Dashboard',
-      isActive: true
-    },
-    {
-      href: '/health-pillars',
-      label: 'Health Pillars',
-      isActive: true
-    },
-    {
-      href: '/tables',
-      label: 'Tables',
-      isActive: true
-    },
-    {
-      href: '/about',
-      label: 'About',
-      isActive: true
-    }];
+    this.items = [
+      {
+        href: '/dashboard',
+        label: 'Dashboard',
+        isActive: true
+      },
+      // {   href: '/health-pillars',   label: 'Health Pillars',   isActive: true },
+      {
+        href: '/tables',
+        label: 'Tables',
+        isActive: true
+      }, {
+        href: '/about',
+        label: 'About',
+        isActive: true
+      }
+    ];
 
   }
 
   onItemClicked(clickedItem) {
-    this.items = this.items.map((item) => {
-      item.isActive = item.label === clickedItem.label;
-      return item;
-    });
+    this.items = this
+      .items
+      .map((item) => {
+        item.isActive = item.label === clickedItem.label;
+        return item;
+      });
   }
 }
 
