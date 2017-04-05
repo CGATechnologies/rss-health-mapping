9f3f0b412a869f4184f5656ccc1697f7095114e6
Joshua Swiss <swissjoshua@gmail.com>
2017-04-05 13:50:56 +0300

    dropdowns working on navbar

--- src/features/dashboard/dash.routes.js
+++ src/features/dashboard/dash.routes.js
@@ -1,35 +1,30 @@
-routes.$inject = [ '$stateProvider' ];
+routes.$inject = ['$stateProvider'];
 
 export default function routes($stateProvider) {
-  $stateProvider
-    .state('dashboard', {
-      url: '/dashboard',
-      template: require('./dash.html'),
-      controller: 'DashboardController',
-      controllerAs: 'dashboard'
-    })
-    .state('hris', {
-      url: '/dashboard/hris',
-      template: require('./dash.hris.html'),
-      controller: 'DashboardController',
-      controllerAs: 'dashboard'
-    })
-    .state('pmis', {
-      url: '/dashboard/pmis',
-      template: require('./dash.pmis.html'),
-      controller: 'DashboardController',
-      controllerAs: 'dashboard'
-    })
-    .state('dhis', {
-      url: '/dashboard/dhis',
-      template: require('./dash.dhis.html'),
-      controller: 'DashboardController',
-      controllerAs: 'dashboard'
-    })
-    .state('surveys', {
-      url: '/dashboard/surveys',
-      template: require('./dash.surveys.html'),
-      controller: 'DashboardController',
-      controllerAs: 'dashboard'
-    });
+  $stateProvider.state('dashboard', {
+    url: '/dashboard',
+    template: require('./dash.html'),
+    controller: 'DashboardController',
+    controllerAs: 'dashboard'
+  }).state('hris', {
+    url: '/dashboard/hris',
+    template: require('./dash.hris.html'),
+    controller: 'DashboardController',
+    controllerAs: 'dashboard'
+  }).state('pmis', {
+    url: '/dashboard/pmis',
+    template: require('./dash.pmis.html'),
+    controller: 'DashboardController',
+    controllerAs: 'dashboard'
+  }).state('hmis', {
+    url: '/dashboard/hmis',
+    template: require('./dash.hmis.html'),
+    controller: 'DashboardController',
+    controllerAs: 'dashboard'
+  }).state('surveys', {
+    url: '/dashboard/surveys',
+    template: require('./dash.surveys.html'),
+    controller: 'DashboardController',
+    controllerAs: 'dashboard'
+  });
 }
